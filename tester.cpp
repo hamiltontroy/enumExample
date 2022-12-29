01696719740f4e616c6d02b4441d0c9ba0126b32
Troy Hamilton <110515824+hamiltontroy@users.noreply.github.com>
2022-12-29 17:09:10 -0600

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -3,15 +3,15 @@
 void yort()
 {
     enum
-{
-    get,        // set to 0 at compile time
-    post,       // set to 1 at compile time
-    foo = 999,  // set to 999 at compile time
-    bar,        // set to 1000 at compile time
-    kaf,        // set to 1001 at compile time
-    cat = 88,   // set to 88 at compile time
-    dog         // set to 89 at compile time
-};
+    {
+        get,        // set to 0 at compile time
+        post,       // set to 1 at compile time
+        foo = 999,  // set to 999 at compile time
+        bar,        // set to 1000 at compile time
+        kaf,        // set to 1001 at compile time
+        cat = 88,   // set to 88 at compile time
+        dog         // set to 89 at compile time
+    };
     
     printf("%d\n", kaf);    // prints 1001
 }
